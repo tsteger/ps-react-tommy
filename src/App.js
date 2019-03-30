8ad3634095b0dd64f6c0841ca8f29445dee6626c
tsteger <tommysteger@gmail.com>
2019-03-30 22:59:08 +0100

    del

--- src/App.js
+++ src/App.js
@@ -1,72 +1,44 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
-import { Power } from './components/Test';
-import { Form } from './components/Form';
-import { CardList } from './components/Card';
+import React, { Component } from "react";
+import { Route } from "react-router";
+import { Layout } from "./components/Layout";
 
-import { VitecFetch } from './components/VitecFetch';
+import { VitecFetch } from "./components/VitecFetch";
 
-import { Rubrik } from './components/Rubrik';
-import { CheckboxContainer } from './components/CheckboxContainer';
-import { OtherInformation } from './components/OtherInformation';
+import { Rubrik } from "./components/Rubrik";
+import { CheckboxContainer } from "./components/chkbox/CheckboxContainer";
+import { OtherInformation } from "./components/OtherInformation";
 
-import './css/css.css';
-//import axios from 'axios';
-
-const testData = [
-    { name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook" },
-    { name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu" },
-    { name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook" },
-];
+import "./css/css.css";
 
 // Back to basic
 
 export default class App extends Component {
-    static displayName = App.name;
-    constructor(props) {
-        super(props);
-        this.state = {
-            profiles: testData,
-        };
-    }
-    render() {
-        return (
-            <Layout>
-                <div>
-                    <Rubrik rubrik="Best&auml;llningsinformation" />
-                    <CheckboxContainer />
-                    <OtherInformation />
-                    
-                </div>
-                <Route path='/VitecFetch' component={VitecFetch} />  
-                    {/*
-                        <Power />
-                    <div className="header">{this.props.title}</div>
-                    <Form />
-                    <CardList profiles={this.state.profiles} />
-               
-                <Route exact path='/' component={Home} />
-                <Route path='/counter' component={Counter} />
-                <Route path='/fetch-data' component={FetchData} />                
-                <Route path='/Card' component={CardList} />
-                    <Route path='/CheckboxContainer' component={CheckboxContainer} />
-                    */}
-
-            </Layout>
-
-        );
-    }
+  static displayName = App.name;
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <Layout>
+        <div>
+          <Rubrik rubrik="Best&auml;llningsinformation" />
+          <CheckboxContainer />
+          <OtherInformation />
+        </div>
+        <Route path="/VitecFetch" component={VitecFetch} />
+      </Layout>
+    );
+  }
 }
-{/*
- * Å Ä Ö codes if neaded
-å & aring;  &#229;
-ä & auml;   &#228;
-ö & ouml;   &#246;
-Å & Aring;  &#197;
-Ä & Auml;   &#196;
-Ö & Ouml;   &#214;
-*/}
+{
+  /*
+ * ï¿½ ï¿½ ï¿½ codes if neaded
+ï¿½ & aring;  &#229;
+ï¿½ & auml;   &#228;
+ï¿½ & ouml;   &#246;
+ï¿½ & Aring;  &#197;
+ï¿½ & Auml;   &#196;
+ï¿½ & Ouml;   &#214;
+*/
+}
---
+++ src/components/chkbox/CheckboxContainer.js
@@ -0,0 +1,54 @@
+﻿import React from "react";
+//import PropTypes from 'prop-types';
+import checkboxes from "./checkboxes";
+import Checkbox from "./checkbox";
+
+import { Container } from "reactstrap";
+import { Row } from "reactstrap";
+import { Col } from "reactstrap";
+import "./chkbox.css";
+export class CheckboxContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      checkedItems: new Map()
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const item = e.target.name;
+    const isChecked = e.target.checked;
+    this.setState(prevState => ({
+      checkedItems: prevState.checkedItems.set(item, isChecked)
+    }));
+    console.log(e.target.checked);
+  }
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <React.Fragment>
+            {checkboxes.map(item => (
+              <div className="chk_box_container" key={item.key}>
+                <Col md={12} lg={6}>
+                  <label className="form_label" key={item.key}>
+                    <Checkbox
+                      name={item.name}
+                      checked={this.state.checkedItems.get(item.name)}
+                      onChange={this.handleChange}
+                    />
+                    <span className="chk_box_text">{item.name}</span>
+                  </label>
+                </Col>
+              </div>
+            ))}
+          </React.Fragment>
+        </Row>
+      </Container>
+    );
+  }
+}
